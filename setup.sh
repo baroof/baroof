2b136a91951b33edccdf28285ca6099bc9d99345
Will Mooreston <will.mooreston@gmail.com>
2021-09-12 11:05:07 -0400

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# initial set of tools I like. Assumes recent Mac OS.
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brews
+brew install go node ruby postgresql sqllite yarn
+brew install --cask iterm2
+brew install --cask docker

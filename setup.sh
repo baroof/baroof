618d1536b61cd8f76bc1b650e3dc2f67a7c387eb
Will Mooreston <will.mooreston@gmail.com>
2021-09-12 11:21:15 -0400

    more tools for setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brews
-brew install go node ruby postgresql sqllite yarn
-brew install --cask iterm2
-brew install --cask docker
+# formulae & casks
+brew install go node ruby postgresql sqlite yarn
+brew install --cask brave-browser docker google-chrome iterm2 slack
+
+# manual installs (for now)
+# https://code.visualstudio.com/docs/setup/mac
+#   * Cmd+Shift+P > shell command > "Install 'code' command in PATH"
+# https://tapbots.com/pastebot/
+# 

c7c8c1df7fbba0b6d2dd8bce09f8df0efe98e35e
Will Mooreston <will.mooreston@gmail.com>
2021-09-12 11:43:04 -0400

    added ohmyzsh, p10k to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 # initial set of tools I like. Assumes recent Mac OS.
 
+mkdir -p ~/dev/tools
+
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# formulae & casks
 brew install go node ruby postgresql sqlite yarn
 brew install --cask brave-browser docker google-chrome iterm2 slack
+
+# ohmyzsh, p10k, etc
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/dev/tools/
+cp .zshrc ~/
+cp .p10k.zsh ~/
+source ~/.zshrc
 
 # manual installs (for now)
 # https://code.visualstudio.com/docs/setup/mac
